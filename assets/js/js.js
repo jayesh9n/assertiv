c4974c6798312a65322a75ddda07abec65f69b7f
jayesh <jayesh9n@gmail.com>
2024-06-09 14:58:53 +0530

    ui changes

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,11 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Adjust this value as needed
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
+});
 $('.client-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -17,6 +25,55 @@ $('.client-carousel').owlCarousel({
         },
         991:{
             items:4,  
+        }
+    }
+})
+$('.trusted-client').owlCarousel({
+    loop:true, 
+    margin:20,
+    dots:false,
+    nav:false,
+    autoplay:false,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1, 
+        },
+        768:{
+            items:2, 
+        },
+        991:{
+            items:4,  
+        },
+        1200:{
+            items:5,  
+        }
+    }
+})
+$('.trusted-client1').owlCarousel({
+    loop:true, 
+    margin:20,
+    rtl:true,
+    dots:false,
+    nav:false,
+    autoplay:false,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1, 
+        },
+        768:{
+            items:2, 
+        },
+        991:{
+            items:4,  
+        },
+        1200:{
+            items:5,  
         }
     }
 })
@@ -52,3 +109,36 @@ $(document).ready(function() {
 
 
 
+
+$(document).ready(function() {
+    let counterSection = $('#counter-section');
+    let counters = $('.counter');
+    let counted = false;
+
+    $(window).on('scroll', function() {
+        let scrollPosition = $(window).scrollTop() + $(window).height();
+        let sectionOffset = counterSection.offset().top;
+
+        if (!counted && scrollPosition >= sectionOffset) {
+            counted = true;
+            counters.each(function() {
+                let counter = $(this);
+                let target = +counter.attr('data-target');
+                let countTo = target;
+                $({ countNum: counter.text() }).animate({
+                    countNum: countTo
+                },
+                {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function() {
+                        counter.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        counter.text(this.countNum + '+');
+                    }
+                });
+            });
+        }
+    });
+});
